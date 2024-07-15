6ba35d7524842b244310ed09c870e8319440f186
Şeyma G <104601325+Seymagocmez@users.noreply.github.com>
2024-07-15 23:32:58 +0300

    rrlrae astronomical 1st analysis

---
+++ rrlrae.py
@@ -0,0 +1,537 @@
+
+import importlib.util
+import arviz as az
+import matplotlib.pyplot as plt
+import numpy as np
+import pymc3 as pm
+from matplotlib import pyplot as plt
+from matplotlib.text import Text
+
+from astroML.datasets import fetch_rrlyrae_templates
+
+from scipy.signal import fftconvolve
+
+from scipy import fftpack
+from matplotlib import mlab
+
+from astroML.datasets import fetch_LIGO_large
+
+from scipy.fftpack import fft
+from scipy.stats import norm
+
+from astroML.fourier import PSD_continuous
+from astroML.plotting import setup_text_plots
+
+templates = fetch_rrlyrae_templates()
+x, y = templates['115r'].T
+fig = plt.figure(figsize=(5, 5))
+fig.subplots_adjust(hspace=0)
+
+
+kvals = [1, 3, 8]
+subplots = [311, 312, 313]
+
+for (k, subplot) in zip(kvals, subplots):
+    ax = fig.add_subplot(subplot)
+
+    # Use FFT to fit a truncated Fourier series
+    y_fft = np.fft.fft(y)
+    y_fft[k + 1:-k] = 0
+    y_fit = np.fft.ifft(y_fft).real
+
+    # plot the true value and the k-term reconstruction
+    ax.plot(np.concatenate([x, 1 + x]),
+            np.concatenate([y, y]), '--k', lw=2)
+    ax.plot(np.concatenate([x, 1 + x]),
+            np.concatenate([y_fit, y_fit]), color='gray')
+
+    label = "%i mode" % k
+    if k > 1:
+        label += 's'
+
+    ax.text(0.02, 0.1, label, ha='left', va='bottom',
+            transform=ax.transAxes)
+
+    if subplot == subplots[-1]:
+        ax.set_xlabel('phase')
+    else:
+        ax.xaxis.set_major_formatter(plt.NullFormatter())
+
+    if subplot == subplots[1]:
+        ax.set_ylabel('amplitude')
+    ax.yaxis.set_major_formatter(plt.NullFormatter())
+
+    ax.set_xlim(0, 2)
+    ax.set_ylim(1.1, -0.1)
+
+plt.show()
+# Generate data
+np.random.seed(1)
+x = np.linspace(0, 1, 500)
+h = 0.01
+C = np.exp(-0.5 * (x - x[:, None]) ** 2 / h ** 2)
+y = 0.8 + 0.3 * np.random.multivariate_normal(np.zeros(len(x)), C)
+
+# define function
+w = np.zeros_like(x)
+w[(x > 0.12) & (x < 0.28)] = 1
+# Set up the plots
+fig = plt.figure(figsize=(5, 5))
+ax1 = fig.add_subplot(111)
+
+# plot the data and window function
+ax1.plot(x, y, '-k', label=r'data $D(x)$')
+ax1.fill(x, w, color='gray', alpha=0.5,
+        label=r'window $W(x)$')
+ax1.fill(x, w[::-1], color='gray', alpha=0.5)
+
+ax1.legend()
+ax1.xaxis.set_major_formatter(plt.NullFormatter())
+
+ax1.set_xlabel('$x$')
+ax1.set_ylabel('$D$')
+ax1.set_xlim(0.01, 0.99)
+ax1.set_ylim(0.0, 2.0)
+(0.0, 2.0)
+
+plt.show()
+# Generate data
+np.random.seed(1)
+x = np.linspace(0, 1, 500)
+h = 0.01
+C = np.exp(-0.5 * (x - x[:, None]) ** 2 / h ** 2)
+y = 0.8 + 0.3 * np.random.multivariate_normal(np.zeros(len(x)), C)
+
+# define function
+w = np.zeros_like(x)
+w[(x > 0.12) & (x < 0.28)] = 1
+
+# Set up the plots
+fig = plt.figure(figsize=(5, 5))
+ax1 = fig.add_subplot(111)
+
+# plot the data and window function
+ax1.plot(x, y, '-k', label=r'data $D(x)$')
+ax1.fill(x, w, color='gray', alpha=0.5,
+        label=r'window $W(x)$')
+ax1.fill(x, w[::-1], color='gray', alpha=0.5)
+
+ax1.legend()
+ax1.xaxis.set_major_formatter(plt.NullFormatter())
+
+ax1.set_xlabel('$x$')
+ax1.set_ylabel('$D$')
+
+ax1.set_xlim(0.01, 0.99)
+ax1.set_ylim(0.0, 2.0)
+(0.0, 2.0)
+y_fft = np.fft.fft(y)
+w_fft = np.fft.fft(w)
+
+yw_fft = y_fft * w_fft
+yw_final = np.fft.ifft(yw_fft)
+# Set up the plots
+fig = plt.figure(figsize=(5, 5))
+
+# plot the Fourier transforms
+N = len(x)
+k = - 0.5 * N + np.arange(N) * 1. / N / (x[1] - x[0])
+
+ax = fig.add_subplot(211)
+ax.plot(k, abs(np.fft.fftshift(y_fft)), '-k')
+
+ax.text(0.95, 0.95, r'$\mathcal{F}(D)$',
+        ha='right', va='top', transform=ax.transAxes)
+
+ax.set_xlim(-100, 100)
+ax.set_ylim(-5, 85)
+
+ax.xaxis.set_major_formatter(plt.NullFormatter())
+
+ax = fig.add_subplot(212)
+ax.plot(k, abs(np.fft.fftshift(w_fft)), '-k')
+
+ax.text(0.95, 0.95,  r'$\mathcal{F}(W)$', ha='right', va='top',
+        transform=ax.transAxes)
+
+ax.set_xlim(-100, 100)
+ax.set_ylim(-5, 85)
+
+ax.set_xlabel('$x$')
+Text(0.5, 0, '$x$')
+
+fig = plt.figure(figsize=(5, 5))
+ax = fig.add_subplot(111)
+ax.plot(k, abs(np.fft.fftshift(yw_fft)), '-k')
+
+ax.text(0.95, 0.95, ('Pointwise\nproduct:\n' +
+                     r'$\mathcal{F}(D) \cdot \mathcal{F}(W)$'),
+        ha='right', va='top', transform=ax.transAxes,
+        bbox=dict(fc='w', ec='k'), zorder=2)
+
+ax.set_xlim(-100, 100)
+ax.set_ylim(-100, 3500)
+
+ax.set_xlabel('$k$')
+Text(0.5, 0, '$k$')
+
+y_norm = np.convolve(np.ones_like(y), w, mode='full')
+valid_indices = (y_norm != 0)
+y_norm = y_norm[valid_indices]
+
+y_w = np.convolve(y, w, mode='full')[valid_indices] / y_norm
+
+# trick: convolve with x-coordinate to find the center of the window at
+#        each point.
+x_w = np.convolve(x, w, mode='full')[valid_indices] / y_norm
+
+# plot the convolution
+fig = plt.figure(figsize=(5, 5))
+ax = fig.add_subplot(111)
+ax.plot(x_w, y_w, '-k')
+
+ax.text(0.5, 0.95, "Convolution:\n" + r"$[D \ast W](x)$",
+        ha='center', va='top', transform=ax.transAxes,
+        bbox=dict(fc='w', ec='k'), zorder=2)
+
+ax.text(0.5, 0.05,
+        (r'$[D \ast W](x)$' +
+         r'$= \mathcal{F}^{-1}\{\mathcal{F}[D] \cdot \mathcal{F}[W]\}$'),
+        ha='center', va='bottom', transform=ax.transAxes)
+
+ax.set_xlabel('$x$')
+ax.set_ylabel('$D_W$')
+
+ax.set_xlim(0.01, 0.99)
+ax.set_ylim(0, 1.99)
+
+(0.0, 1.99)
+
+# Set up the plots
+fig = plt.figure(figsize=(5, 5))
+fig.subplots_adjust(left=0.09, bottom=0.09, right=0.95, top=0.95,
+                    hspace=0.05, wspace=0.05)
+
+#----------------------------------------
+# plot the data and window function
+ax = fig.add_subplot(221)
+ax.plot(x, y, '-k', label=r'data $D(x)$')
+ax.fill(x, w, color='gray', alpha=0.5,
+        label=r'window $W(x)$')
+ax.fill(x, w[::-1], color='gray', alpha=0.5)
+
+ax.legend()
+ax.xaxis.set_major_formatter(plt.NullFormatter())
+
+ax.set_ylabel('$D$')
+
+ax.set_xlim(0.01, 0.99)
+ax.set_ylim(0, 2.0)
+
+#----------------------------------------
+# plot the convolution
+ax = fig.add_subplot(223)
+ax.plot(x_w, y_w, '-k')
+
+ax.text(0.5, 0.95, "Convolution:\n" + r"$[D \ast W](x)$",
+        ha='center', va='top', transform=ax.transAxes,
+        bbox=dict(fc='w', ec='k'), zorder=2)
+
+ax.text(0.5, 0.05,
+        (r'$[D \ast W](x)$' +
+         r'$= \mathcal{F}^{-1}\{\mathcal{F}[D] \cdot \mathcal{F}[W]\}$'),
+        ha='center', va='bottom', transform=ax.transAxes)
+
+for x_loc in (0.2, 0.8):
+    y_loc = y_w[x_w <= x_loc][-1]
+    ax.annotate('', (x_loc, y_loc), (x_loc, 2.0), zorder=1,
+                arrowprops=dict(arrowstyle='->', color='gray', lw=2))
+
+ax.set_xlabel('$x$')
+ax.set_ylabel('$D_W$')
+
+ax.set_xlim(0.01, 0.99)
+ax.set_ylim(0, 1.99)
+
+#----------------------------------------
+# plot the Fourier transforms
+N = len(x)
+k = - 0.5 * N + np.arange(N) * 1. / N / (x[1] - x[0])
+
+ax = fig.add_subplot(422)
+ax.plot(k, abs(np.fft.fftshift(y_fft)), '-k')
+
+ax.text(0.95, 0.95, r'$\mathcal{F}(D)$',
+        ha='right', va='top', transform=ax.transAxes)
+
+ax.set_xlim(-100, 100)
+ax.set_ylim(-5, 85)
+
+ax.xaxis.set_major_formatter(plt.NullFormatter())
+ax.yaxis.set_major_formatter(plt.NullFormatter())
+
+ax = fig.add_subplot(424)
+ax.plot(k, abs(np.fft.fftshift(w_fft)), '-k')
+
+ax.text(0.95, 0.95,  r'$\mathcal{F}(W)$', ha='right', va='top',
+        transform=ax.transAxes)
+
+ax.set_xlim(-100, 100)
+ax.set_ylim(-5, 85)
+
+ax.xaxis.set_major_formatter(plt.NullFormatter())
+ax.yaxis.set_major_formatter(plt.NullFormatter())
+
+#----------------------------------------
+# plot the product of Fourier transforms
+ax = fig.add_subplot(224)
+ax.plot(k, abs(np.fft.fftshift(yw_fft)), '-k')
+
+ax.text(0.95, 0.95, ('Pointwise\nproduct:\n' +
+                     r'$\mathcal{F}(D) \cdot \mathcal{F}(W)$'),
+        ha='right', va='top', transform=ax.transAxes,
+        bbox=dict(fc='w', ec='k'), zorder=2)
+
+ax.set_xlim(-100, 100)
+ax.set_ylim(-100, 3500)
+
+ax.set_xlabel('$k$')
+
+ax.yaxis.set_major_formatter(plt.NullFormatter())
+
+#------------------------------------------------------------
+# Plot flow arrows
+ax = fig.add_axes([0, 0, 1, 1], xticks=[], yticks=[], frameon=False)
+
+arrowprops = dict(arrowstyle="simple",
+                  color="gray", alpha=0.5,
+                  shrinkA=5, shrinkB=5,
+                  patchA=None,
+                  patchB=None,
+                  connectionstyle="arc3,rad=-0.35")
+
+ax.annotate('', [0.57, 0.57], [0.47, 0.57],
+            arrowprops=arrowprops,
+            transform=ax.transAxes)
+ax.annotate('', [0.57, 0.47], [0.57, 0.57],
+            arrowprops=arrowprops,
+            transform=ax.transAxes)
+ax.annotate('', [0.47, 0.47], [0.57, 0.47],
+            arrowprops=arrowprops,
+            transform=ax.transAxes)
+
+plt.show()
+plt.show()
+
+def gaussian(x, a=1.0):
+    return np.exp(-0.5 * (x / a) ** 2)
+
+
+def gaussian_FT(f, a=1.0):
+    return np.sqrt(2 * np.pi * a ** 2) * np.exp(-2 * (np.pi * a * f) ** 2)
+
+a = 1.0
+t = np.linspace(-5, 5, 1000)
+h = gaussian(t, a)
+
+f = np.linspace(-2, 2, 1000)
+H = gaussian_FT(f, a)
+
+# Two plots: one well-sampled, one over-sampled
+N = 12
+
+def plot_aliasing(dt):
+    """
+
+    Parameters
+    ----------
+
+    dt : float
+       
+
+    """
+    # define time-space sampling
+    t_sample = dt * (np.arange(N) - N / 2)
+    h_sample = gaussian(t_sample, a)
+
+    # Fourier transform of time-space sampling
+    df = 1. / dt
+    f_sample = df * (np.arange(N) - N / 2)
+
+    # Plot the results
+    fig = plt.figure(figsize=(7, 3.5))
+    fig.subplots_adjust(left=0.07, right=0.95, wspace=0.16,
+                        bottom=0.1, top=0.85, hspace=0.05)
+
+    # First plot: sampled time-series
+    ax = fig.add_subplot(221)
+    ax.plot(t, h, '-k')
+
+    for ts in t_sample:
+        ax.annotate('', (ts, 0.5), (ts, 0), ha='center', va='center',
+                    arrowprops=dict(arrowstyle='->'))
+    ax.text(0.03, 0.95,
+            ("Signal and Sampling Window\n" +
+             r"Sampling Rate $\Delta t$"),
+            ha='left', va='top', transform=ax.transAxes)
+    ax.set_ylabel('$h(t)$')
+    ax.set_xlim(-5, 5)
+    ax.set_ylim(0, 1.4)
+    ax.xaxis.set_major_formatter(plt.NullFormatter())
+    ax.yaxis.set_major_formatter(plt.NullFormatter())
+    ax.set_title('Time Domain: Multiplication')
+
+    # second plot: frequency space
+    ax = fig.add_subplot(222)
+    ax.plot(f, H, '-k')
+    for fs in f_sample:
+        ax.annotate('', (fs, 1.5), (fs, 0), ha='center', va='center',
+                    arrowprops=dict(arrowstyle='->'))
+    ax.text(0.03, 0.95,
+            ("FT of Signal and Sampling Window\n" +
+             r"$\Delta f = 1 / \Delta t$"),
+            ha='left', va='top', transform=ax.transAxes)
+    ax.set_ylabel('$H(f)$')
+    ax.set_xlim(-1.5, 1.5)
+    ax.set_ylim(0, 3.8)
+    ax.xaxis.set_major_formatter(plt.NullFormatter())
+    ax.yaxis.set_major_formatter(plt.NullFormatter())
+    ax.set_title('Frequency Domain: Convolution')
+
+    # third plot: windowed function
+    ax = fig.add_subplot(223)
+    for (ts, hs) in zip(t_sample, h_sample):
+        if hs < 0.1:
+            continue
+        ax.annotate('', (ts, hs), (ts, 0), ha='center', va='center',
+                    arrowprops=dict(arrowstyle='->'))
+    ax.plot(t, h, ':k')
+    ax.text(0.03, 0.95, "Sampled signal: pointwise\nmultiplication",
+            ha='left', va='top', transform=ax.transAxes)
+    ax.set_xlabel('$t$')
+    ax.set_ylabel('$h(t)$')
+    ax.set_xlim(-5, 5)
+    ax.set_ylim(0, 1.4)
+    ax.xaxis.set_major_formatter(plt.NullFormatter())
+    ax.yaxis.set_major_formatter(plt.NullFormatter())
+
+    # fourth plot: convolved PSD
+    ax = fig.add_subplot(224)
+    window = np.array([gaussian_FT(f - fs, a) for fs in f_sample])
+    ax.plot(f, window.sum(0), '-k')
+    if dt > 1:
+        ax.plot(f, window.T, ':k')
+    ax.text(0.03, 0.95, "Convolution of signal FT\nand window FT",
+            ha='left', va='top', transform=ax.transAxes)
+    ax.set_xlabel('$f$')
+    ax.set_ylabel('$H(f)$')
+    ax.set_xlim(-1.5, 1.5)
+    ax.set_ylim(0, 3.8)
+    ax.xaxis.set_major_formatter(plt.NullFormatter())
+    ax.yaxis.set_major_formatter(plt.NullFormatter())
+
+    if dt > 1:
+        fig.suptitle(r"Undersampled data: $\Delta t > t_c$")
+    else:
+        fig.suptitle(r"Well-sampled data: $\Delta t < t_c$")
+
+        # Two plots: one well-sampled, one over-sampled
+N = 12
+
+def plot_aliasing(dt):
+    """
+
+    Parameters
+    ----------
+
+    dt : float
+       
+
+    """
+    # define time-space sampling
+    t_sample = dt * (np.arange(N) - N / 2)
+    h_sample = gaussian(t_sample, a)
+
+    # Fourier transform of time-space sampling
+    df = 1. / dt
+    f_sample = df * (np.arange(N) - N / 2)
+
+    # Plot the results
+    fig = plt.figure(figsize=(7, 3.5))
+    fig.subplots_adjust(left=0.07, right=0.95, wspace=0.16,
+                        bottom=0.1, top=0.85, hspace=0.05)
+
+    # First plot: sampled time-series
+    ax = fig.add_subplot(221)
+    ax.plot(t, h, '-k')
+
+    for ts in t_sample:
+        ax.annotate('', (ts, 0.5), (ts, 0), ha='center', va='center',
+                    arrowprops=dict(arrowstyle='->'))
+    ax.text(0.03, 0.95,
+            ("Signal and Sampling Window\n" +
+             r"Sampling Rate $\Delta t$"),
+            ha='left', va='top', transform=ax.transAxes)
+    ax.set_ylabel('$h(t)$')
+    ax.set_xlim(-5, 5)
+    ax.set_ylim(0, 1.4)
+    ax.xaxis.set_major_formatter(plt.NullFormatter())
+    ax.yaxis.set_major_formatter(plt.NullFormatter())
+    ax.set_title('Time Domain: Multiplication')
+
+    # second plot: frequency space
+    ax = fig.add_subplot(222)
+    ax.plot(f, H, '-k')
+    for fs in f_sample:
+        ax.annotate('', (fs, 1.5), (fs, 0), ha='center', va='center',
+                    arrowprops=dict(arrowstyle='->'))
+    ax.text(0.03, 0.95,
+            ("FT of Signal and Sampling Window\n" +
+             r"$\Delta f = 1 / \Delta t$"),
+            ha='left', va='top', transform=ax.transAxes)
+    ax.set_ylabel('$H(f)$')
+    ax.set_xlim(-1.5, 1.5)
+    ax.set_ylim(0, 3.8)
+    ax.xaxis.set_major_formatter(plt.NullFormatter())
+    ax.yaxis.set_major_formatter(plt.NullFormatter())
+    ax.set_title('Frequency Domain: Convolution')
+
+    # third plot: windowed function
+    ax = fig.add_subplot(223)
+    for (ts, hs) in zip(t_sample, h_sample):
+        if hs < 0.1:
+            continue
+        ax.annotate('', (ts, hs), (ts, 0), ha='center', va='center',
+                    arrowprops=dict(arrowstyle='->'))
+    ax.plot(t, h, ':k')
+    ax.text(0.03, 0.95, "Sampled signal: pointwise\nmultiplication",
+            ha='left', va='top', transform=ax.transAxes)
+    ax.set_xlabel('$t$')
+    ax.set_ylabel('$h(t)$')
+    ax.set_xlim(-5, 5)
+    ax.set_ylim(0, 1.4)
+    ax.xaxis.set_major_formatter(plt.NullFormatter())
+    ax.yaxis.set_major_formatter(plt.NullFormatter())
+
+    # fourth plot: convolved PSD
+    ax = fig.add_subplot(224)
+    window = np.array([gaussian_FT(f - fs, a) for fs in f_sample])
+    ax.plot(f, window.sum(0), '-k')
+    if dt > 1:
+        ax.plot(f, window.T, ':k')
+    ax.text(0.03, 0.95, "Convolution of signal FT\nand window FT",
+            ha='left', va='top', transform=ax.transAxes)
+    ax.set_xlabel('$f$')
+    ax.set_ylabel('$H(f)$')
+    ax.set_xlim(-1.5, 1.5)
+    ax.set_ylim(0, 3.8)
+    ax.xaxis.set_major_formatter(plt.NullFormatter())
+    ax.yaxis.set_major_formatter(plt.NullFormatter())
+
+    if dt > 1:
+        fig.suptitle(r"Undersampled data: $\Delta t > t_c$")
+    else:
+        fig.suptitle(r"Well-sampled data: $\Delta t < t_c$")
+
+        plot_aliasing(0.9)
+
