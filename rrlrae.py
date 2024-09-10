31bdfbad570f77f27a6e62414cb0971287001f61
Şeyma G <104601325+Seymagocmez@users.noreply.github.com>
2024-09-10 11:52:17 +0300

    Time Series analysis complete

--- rrlrae.py
+++ rrlrae.py
@@ -433,3 +433,125 @@ def plot_aliasing(dt):
         fig.suptitle(r"Well-sampled data: $\Delta t < t_c$")
 plot_aliasing(0.9) 
 plt.show()
+plot_aliasing(1.2)
+plt.show()
+Nbins = 2 ** 15
+Nobs = 40
+f = lambda t: np.sin(np.pi * t / 3)
+
+t = np.linspace(-100, 200, Nbins)
+dt = t[1] - t[0]
+y = f(t)
+# select observations
+np.random.seed(42)
+t_obs = 100 * np.random.random(40)
+
+D = abs(t_obs[:, np.newaxis] - t)
+i = np.argmin(D, 1)
+
+t_obs = t[i]
+y_obs = y[i]
+window = np.zeros(Nbins)
+window[i] = 1
+
+# Compute PSDs
+Nfreq = int(Nbins / 2)
+
+dt = t[1] - t[0]
+df = 1. / (Nbins * dt)
+f = df * np.arange(Nfreq)
+
+PSD_window = abs(np.fft.fft(window)[:Nfreq]) ** 2
+PSD_y = abs(np.fft.fft(y)[:Nfreq]) ** 2
+PSD_obs = abs(np.fft.fft(y * window)[:Nfreq]) ** 2
+PSD_window /= 500
+PSD_y /= PSD_y.max()
+PSD_obs /= 500
+# Prepare the figures
+fig = plt.figure(figsize=(8, 4))
+fig.subplots_adjust(bottom=0.15, hspace=0.2, wspace=0.25,
+                    left=0.12, right=0.95)
+
+# First panel: data vs time
+ax = fig.add_subplot(221)
+ax.plot(t, y, '-', c='gray')
+ax.plot(t_obs, y_obs, '.k', ms=4)
+ax.text(0.95, 0.93, "Data", ha='right', va='top', transform=ax.transAxes)
+ax.set_ylabel('$y(t)$')
+ax.set_xlim(0, 100)
+ax.set_ylim(-1.5, 1.8)
+
+# Second panel: PSD of data
+ax = fig.add_subplot(222)
+ax.fill(f, PSD_y, fc='gray', ec='gray')
+ax.plot(f, PSD_obs, '-', c='black')
+ax.text(0.95, 0.93, "Data PSD", ha='right', va='top', transform=ax.transAxes)
+ax.set_ylabel('$P(f)$')
+ax.set_xlim(0, 1.0)
+ax.set_ylim(-0.1, 1.1)
+
+# Third panel: window vs time
+ax = fig.add_subplot(223)
+ax.plot(t, window, '-', c='black')
+ax.text(0.95, 0.93, "Window", ha='right', va='top', transform=ax.transAxes)
+ax.set_xlabel('$t$')
+ax.set_ylabel('$y(t)$')
+ax.set_xlim(0, 100)
+ax.set_ylim(-0.2, 1.5)
+
+# Fourth panel: PSD of window
+ax = fig.add_subplot(224)
+ax.plot(f, PSD_window, '-', c='black')
+ax.text(0.95, 0.93, "Window PSD", ha='right', va='top', transform=ax.transAxes)
+ax.set_xlabel('$f$')
+ax.set_ylabel('$P(f)$')
+ax.set_xlim(0, 1.0)
+ax.set_ylim(-0.1, 1.1)
+
+plt.show()
+np.random.seed(1)
+
+tj = np.linspace(-25, 25, 512)
+hj = np.sin(tj)
+hj *= norm(0, 10).pdf(tj)
+# plot the results
+fig = plt.figure(figsize=(5, 3.75))
+fig.subplots_adjust(hspace=0.35)
+ax1 = fig.add_subplot(211)
+ax2 = fig.add_subplot(212)
+
+offsets = (0, 0.15)
+colors = ('black', 'gray')
+linewidths = (1, 2)
+errors = (0.005, 0.05)
+
+for (offset, color, error, linewidth) in zip(offsets, colors,
+                                             errors, linewidths):
+    # compute the PSD
+    err = np.random.normal(0, error, size=hj.shape)
+    hj_N = hj + err + offset
+    fk, PSD = PSD_continuous(tj, hj_N)
+
+    # plot the data and PSD
+    ax1.scatter(tj, hj_N, s=4, c=color, lw=0)
+    ax1.plot(tj, 0 * tj + offset, '-', c=color, lw=1)
+    ax2.plot(fk, PSD, '-', c=color, lw=linewidth)
+
+# vertical line marking the expected peak location
+ax2.plot([0.5 / np.pi, 0.5 / np.pi], [-0.1, 1], ':k', lw=1)
+
+ax1.set_xlim(-25, 25)
+ax1.set_ylim(-0.1, 0.3001)
+
+ax1.set_xlabel('$t$')
+ax1.set_ylabel('$h(t)$')
+
+ax1.yaxis.set_major_locator(plt.MultipleLocator(0.1))
+
+ax2.set_xlim(0, 0.8)
+ax2.set_ylim(-0.101, 0.801)
+
+ax2.set_xlabel('$f$')
+ax2.set_ylabel('$PSD(f)$')
+
+plt.show()
